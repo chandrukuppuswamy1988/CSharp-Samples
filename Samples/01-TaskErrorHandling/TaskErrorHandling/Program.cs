a8d91b2534f857e01aab20ba5265e5e723598037
CHANDRU K <chandrukuppuswamy@outlook.com>
2017-10-29 12:27:20 +0530

    initial seed for task error handling
    initial seed for task error handling

---
+++ Samples/01-TaskErrorHandling/TaskErrorHandling/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskErrorHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ShowAggregatedException();
+            Console.ReadLine();
+
+        }
+
+        private static async void ShowAggregatedException()
+        {
+            Task taskResult = null;
+
+            try
+            {
+                Task t1 = ThrowAfter(2000, "first");
+                Task t2 = ThrowAfter(1000, "second");
+
+                await (taskResult = Task.WhenAll(t1, t2));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("handled {0}", ex.Message);
+                foreach (var ex1 in taskResult.Exception.InnerExceptions)
+                {
+                    Console.WriteLine("inner Exception {0}", ex1.Message);
+                }
+            }
+        }
+
+        static async Task ThrowAfter(int ms,string message)
+        {
+            await Task.Delay(ms);
+            throw new Exception(message);
+        }
+
+    }
+}
